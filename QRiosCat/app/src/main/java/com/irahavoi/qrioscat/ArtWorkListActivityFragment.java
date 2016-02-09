3989d1f748360676acee5748ec6bbc03dd596c23
Illia Rahavoi <irahavoi@gmail.com>
2016-02-08 22:34:57 -0600

    Up

--- QRiosCat/app/src/main/java/com/irahavoi/qrioscat/ArtWorkListActivityFragment.java
+++ QRiosCat/app/src/main/java/com/irahavoi/qrioscat/ArtWorkListActivityFragment.java
@@ -2,6 +2,7 @@ package com.irahavoi.qrioscat;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import android.view.ViewGroup;
  * A placeholder fragment containing a simple view.
  */
 public class ArtWorkListActivityFragment extends Fragment {
+    RecyclerView mRecyclerView;
 
     public ArtWorkListActivityFragment() {
     }
@@ -17,6 +19,10 @@ public class ArtWorkListActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_art_work_list, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_art_work_list, container, false);
+
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerview_artworklist);
+
+        return rootView;
     }
 }
